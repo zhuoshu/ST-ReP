c051166a527629612e32740ffe0cc4659b10c0eb
Qi Zheng <zq12756@126.com>
2025-12-15 15:22:41 +0800

    Update prepareWeatherBench.py

--- datasets/prepareWeatherBench.py
+++ datasets/prepareWeatherBench.py
@@ -18,6 +18,10 @@ rdata = xr.open_mfdataset(raw_data_path, combine='by_coords')
 
 raw_data = rdata.__getattr__(attr)
 print(raw_data.shape)
+
+if len(raw_data.shape) == 4: # when there are different level, we choose the 13-th level which is sea level, following [AAAI 2022 CLCRN] (https://github.com/EDAPINENUT/CLCRN/tree/main)
+    raw_data = raw_data[:,-1,...]
+
 start_date = '2016-01-01'
 end_date = '2017-01-01'
 time = rdata.time.values
